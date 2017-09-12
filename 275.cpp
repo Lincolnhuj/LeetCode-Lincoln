ffca35e3b305bf6232baace428d7454748543520
Lincolnhuj <lincolnhuj@gmail.com>
2017-09-12 00:15:52 -0700

    Update 275.cpp

--- 275.cpp
+++ 275.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int l = citations.size();
+        int start = 0;
+        int end = l;
+        while(end>start){
+        	int mid = start + ((end - start)>>1);
+        	if(citations[mid]<l-mid){
+        		start = mid + 1;
+        	}
+        	else {
+        		end = mid;
+        	}
+        }
+        return l-start;
+    }
+};
