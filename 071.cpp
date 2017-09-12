88d02255820136932c2b2fd7cf96d823e6c947a6
Lincolnhuj <lincolnhuj@gmail.com>
2017-09-11 21:26:17 -0700

    Update 071.cpp

--- 071.cpp
+++ 071.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> resultV;
+        int pointer = 0;
+        int l = path.size();
+        while(pointer<l){
+        	pointer++;
+        	string item;
+        	while(pointer<l and path[pointer]!='/'){
+        		item+=path[pointer];
+        		pointer++;
+        	}
+        	if(item == "." or item =="") continue;
+        	if(item == ".."){
+        		if(!resultV.empty()) resultV.pop_back();
+        		continue;
+        	}
+        	resultV.push_back(item);
+        	
+        }
+        // combine items into a result
+        string result = "";
+        for(string item : resultV){
+        	result = result + "/" + item;
+        }
+        if(result == "") result = "/";
+        return result;
+    }
+};
