7ede0f4af27b2f5b685c8ca64520471754cbf63c
Lincolnhuj <lincolnhuj@gmail.com>
2017-09-13 22:48:52 -0700

    Update 553.cpp

--- 553.cpp
+++ 553.cpp
@@ -1,1 +1,26 @@
-
+class Solution {// Lincoln 3ms
+public:
+    string optimalDivision(vector<int>& nums) {
+        unsigned int l = nums.size();
+        string result = "";
+        if(l == 0) return "";
+        else if(l < 3){
+        	for(int i = 0; i < l-1; i++){
+        		result += to_string(nums[i]);
+        		result += "/";
+        	}
+        	result += to_string(nums[l-1]);
+        }
+        else{
+        	result += to_string(nums[0]);
+        	result += "/(";
+        	result += to_string(nums[1]);
+        	for(int i = 2; i < l; i++){
+        		result += "/";
+        		result += to_string(nums[i]);
+        		}
+        	result += ")";	
+        }
+        return result;
+    }
+};
