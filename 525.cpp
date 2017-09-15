2cb138030464803ee357bd1fe1128f37c104e7d3
Lincolnhuj <lincolnhuj@gmail.com>
2017-09-14 19:16:22 -0700

    Update 525.cpp

--- 525.cpp
+++ 525.cpp
@@ -1,1 +1,22 @@
-
+using namespace std;
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        map <int,int> dict;
+        dict[0] = -1;
+        int maxSize = 0;
+        int ones = 0;
+        int l = nums.size();
+        for(int i = 0; i<l; i++){
+        	ones += nums[i];
+        	int overflow = ones - (i+1-ones);
+        	if(dict.count(overflow) == 0){
+        		dict[overflow] = i;
+        	}
+        	else{
+        		maxSize = max(maxSize, i - dict[overflow]);
+        	}
+        }
+        return maxSize;
+    }
+};
