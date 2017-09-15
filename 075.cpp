e81d9d0ed3bb46f956659c2217d986615030fe3b
Lincolnhuj <lincolnhuj@gmail.com>
2017-09-14 19:29:43 -0700

    Update 075.cpp

--- 075.cpp
+++ 075.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = nums.size();
+        for(int i = 0; i<l; i++){
+        	for(int j = 0; j< i; j++){
+        		if(nums[i]<nums[j]) swap(nums[i],nums[j]);
+        	}
+        }
+    }
+};
